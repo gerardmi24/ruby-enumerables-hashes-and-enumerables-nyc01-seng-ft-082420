f683399b7925c79f505b7a3e525fa6d39b93c900
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-14 00:33:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,15 +5,14 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-winner = ""
+
 def select_winner(passengers)
   # add the code snippet here!
-
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   end
-end
 
   winner
